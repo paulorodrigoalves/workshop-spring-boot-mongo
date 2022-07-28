2601c5ebfafc3e89be62bc0cb62d10891181db73
Paulo Rodrigo <paulorodrigo1992@gmail.com>
2022-07-28 10:58:02 -0300

    Conectado ao MongoDB com repository e service

--- src/test/java/com/educandoweb/workshopmongo/WorkshopmongoApplicationTests.java
+++ src/test/java/com/educandoweb/workshopmongo/WorkshopmongoApplicationTests.java
@@ -1,12 +1,12 @@
 package com.educandoweb.workshopmongo;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WorkshopmongoApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
